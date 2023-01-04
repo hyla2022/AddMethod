27470214ab652b259c7601466ccea59dcbf90773
hyinping <1135277318@qq.com>
2023-01-04 09:53:13 +0800

    add comand line hahahah

--- Function/src/main/java/com/test/git/GitTest.java
+++ Function/src/main/java/com/test/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello world!");
         System.out.println("Today have a good weather!");
         System.out.println("**********");
+        System.out.println("hahahahahhah");
     }
 }
