c9230920668804fe4ba0e5809f757c39c448b39c
hyinping <hyinping@vmware.com>
2023-01-04 10:58:00 +0800

    add conflict

--- Function/src/main/java/com/test/git/GitTest.java
+++ Function/src/main/java/com/test/git/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
         System.out.println("**********");
         System.out.println("hahahahahhah");
         System.out.println("=========");
+        System.out.println("conflict");
+
     }
 }
